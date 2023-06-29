cb30f1b475bb0c2abc2abfd6bf33e24839f35006
Jung-kr <jungwy98@gmail.com>
2023-06-29 23:26:54 +0900

    feat : board repository 개발

--- server/src/main/java/com/jung/board/repository/BoardRepository.java
+++ server/src/main/java/com/jung/board/repository/BoardRepository.java
@@ -1,2 +1,9 @@
-package com.jung.board.repository;public class BoardRepository {
+package com.jung.board.repository;
+
+import com.jung.board.domain.Board;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BoardRepository extends JpaRepository<Board, Long> {
 }
