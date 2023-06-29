c1b6866332e7b23be7f1f23abed9e7f55a6d1d05
Jung-kr <jungwy98@gmail.com>
2023-06-29 23:31:55 +0900

    feat : dto 및 에러 핸들링을 위한 ApiError, ApiResult class

---
+++ server/src/main/java/com/jung/board/dto/BoardDeleteDto.java
@@ -0,0 +1,13 @@
+package com.jung.board.dto;
+
+import com.jung.board.domain.Board;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class BoardDeleteDto {
+
+    private Long id;
+
+}
