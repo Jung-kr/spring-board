f6ee55578e40810dd876cd963e93982ef0e85757
Jung-kr <jungwy98@gmail.com>
2023-06-29 23:29:55 +0900

    feat : board service 개발

--- server/src/main/java/com/jung/board/service/BoardService.java
+++ server/src/main/java/com/jung/board/service/BoardService.java
@@ -1,2 +1,41 @@
-package com.jung.board.service;public class BoardService {
+package com.jung.board.service;
+
+import com.jung.board.domain.Board;
+import com.jung.board.repository.BoardRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class BoardService {
+
+    private final BoardRepository boardRepository;
+    public List<Board> findBoards() {
+        return boardRepository.findAll();
+    }
+
+    public Board findOne(Long boardId) {
+        return boardRepository.findById(boardId).orElseThrow(NullPointerException::new);
+    }
+
+    @Transactional // DB에 직접적인 영향을 주기 때문, 안전하게 작동하기 위함
+    public void create(Board board) {
+        boardRepository.save(board);
+    }
+
+    @Transactional
+    public void update(Long id, String title, String content) {
+        Board board = boardRepository.findById(id).orElseThrow(NullPointerException::new);
+        board.setTitle(title);
+        board.setContent(content);
+    }
+
+    @Transactional
+    public void delete(Board board) {
+        boardRepository.delete(board);
+    }
 }
